3d4c47a7276e537a8e9a0496859532cf3d9d72f5
Javy Rodillon <reyfoxconner@gmail.com>
2025-04-13 02:58:21 +0800

    Add CORS origin for Netlify deployment in backend

--- backend/main.py
+++ backend/main.py
@@ -11,11 +11,12 @@ origins = [
     "http://localhost:5173",
     "http://localhost:3000",
     "*",
+     "https://fastapi-rodillon.netlify.app",
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=origins,  # Allow frontend to access the backend
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
